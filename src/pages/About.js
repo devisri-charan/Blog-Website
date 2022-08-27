dc624c3f19e7b579ecd6477161cbff9ae7c0a944
DeviSriCharan20412 <valupadasu20412@iiitd.ac.in>
2022-08-27 23:37:37 +0530

    Added Pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Home = () => {
+const About = () => {
   return (
     <div className="mb-20">
-      <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">Hello Welcome to my Blog</h1>
+      <h1 className="sm:text-4xl text-2xl font-bold my-6 text-gray-900">About</h1>
       <p className="mx-auto leading-relaxed text-base mb-4">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum quaerat
         libero, enim quisquam aperiam recusandae tenetur dignissimos labore
@@ -35,4 +35,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default About;
