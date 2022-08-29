2e85d0a7bfbd7178013aee6cf01a2afaa1310681
DeviSriCharan20412 <valupadasu20412@iiitd.ac.in>
2022-08-29 18:38:26 +0530

    Tested express with postman

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
+// Middleware
+app.use(express.json({extended: false}));
+
 app.get('/',(req,res) => res.send("Hello World!"));
+app.post('/', (req,res) => res.send(`Hello ${req.body.name}`));
 
 app.listen(8000, () => console.log(`Server started at port ${PORT}`));
