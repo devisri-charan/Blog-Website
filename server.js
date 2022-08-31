ef96a5d68ab310cd2ccfdbb3b0f25e409bb6c4c9
DeviSriCharan20412 <valupadasu20412@iiitd.ac.in>
2022-08-31 18:36:50 +0530

    Added MongoDB to Express

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
 const express = require("express");
 const app = express();
+const {MongoClient} = require("mongodb")
 const PORT = process.env.PORT || 8000;
 
-const articlesInfo = {
-    "learn-react": {
-        comments: [],
-    },
-    "learn-node": {
-        comments: [],
-    },
-    "my-thoughts-on-learning-react": {
-        comments: [],
-    },
-};
+
 
 // Middleware
 app.use(express.json({extended: false}));
+
+app.get('/api/articles/:name', async (req,res) => {
+    try {
+        const articleName = req.params.name;
+        const client = await MongoClient.connect('mongodb://localhost:27017');
+        const db = client.db("blog");
+        const articlesInfo = await db.
+            collection('articles')
+            .findOne({name: articleName});
+        res.status(200).json(articlesInfo);
+        client.close();
+    } catch (error) {
+        res.status(500).json({message: "Error connecting to database", error});
+    }
+})
+
 app.post('/api/articles/:name/add-comments', (req,res) => {
     const {username,text} = req.body
     const articleName = req.params.name;
